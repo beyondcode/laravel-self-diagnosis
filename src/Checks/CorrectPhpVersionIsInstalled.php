3743d38c7d431cc96f7b812e939c762222f8eac9
Marcel Pociot <m.pociot@gmail.com>
2018-07-04 21:11:46 +0200

    Lookup required PHP version from composer.json

--- src/Checks/CorrectPhpVersionIsInstalled.php
+++ src/Checks/CorrectPhpVersionIsInstalled.php
@@ -2,8 +2,18 @@
 
 namespace BeyondCode\SelfDiagnosis\Checks;
 
+use Illuminate\Filesystem\Filesystem;
+
 class CorrectPhpVersionIsInstalled implements Check
 {
+    /** @var Filesystem */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
     /**
      * The name of the check.
      *
@@ -17,20 +27,34 @@ class CorrectPhpVersionIsInstalled implements Check
     /**
      * Perform the actual verification of this check.
      *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      * @return bool
      */
     public function check(): bool
     {
-        return version_compare(phpversion(), '7.1.3', '>=');
+        return version_compare(phpversion(), $this->getRequiredPhpVersion(), '>=');
     }
 
     /**
      * The error message to display in case the check does not pass.
      *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      * @return string
      */
     public function message() : string
     {
-        return 'You do not have the required PHP version installed.';
+        return 'You do not have the required PHP version installed.'.PHP_EOL.'Required: '.$this->getRequiredPhpVersion().PHP_EOL.'Used: '.phpversion();
+    }
+
+    /**
+     * @return mixed
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function getRequiredPhpVersion()
+    {
+        $composer = json_decode($this->filesystem->get(base_path('composer.json')), true);
+        $versionString = array_get($composer, 'require.php');
+
+        return str_replace(['^', '~', '<', '>', '='], '', $versionString);
     }
 }
---
+++ tests/CorrectPhpVersionIsInstalledTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BeyondCode\SelfDiagnosis\Tests;
+
+use Orchestra\Testbench\TestCase;
+use BeyondCode\SelfDiagnosis\Checks\CorrectPhpVersionIsInstalled;
+
+class CorrectPhpVersionIsInstalledTest extends TestCase
+{
+    /** @test */
+    public function it_detects_the_php_version_from_composer_json()
+    {
+        $this->app->setBasePath(__DIR__ . '/fixtures');
+
+        $check = app(CorrectPhpVersionIsInstalled::class);
+
+        $this->assertSame('7.1.3', $check->getRequiredPhpVersion());
+    }
+}
