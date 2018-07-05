6062186d6f844fcf012a3ede6990b79373f9df19
Schnoop <thorsten@daners.info>
2018-07-05 14:29:35 +0200

    Use vendor/composer/installed.json instead of composer.json

--- src/Checks/PhpExtensionsAreInstalled.php
+++ src/Checks/PhpExtensionsAreInstalled.php
@@ -2,10 +2,22 @@
 
 namespace BeyondCode\SelfDiagnosis\Checks;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
 
 class PhpExtensionsAreInstalled implements Check
 {
+
+    const EXT = 'ext-';
+
+    /** @var Filesystem */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
     /** @var Collection */
     private $extensions;
 
@@ -24,7 +36,7 @@ class PhpExtensionsAreInstalled implements Check
      *
      * @return string
      */
-    public function message() : string
+    public function message(): string
     {
         return 'The following extensions are missing: '.PHP_EOL.$this->extensions->implode(PHP_EOL);
     }
@@ -45,11 +57,33 @@ class PhpExtensionsAreInstalled implements Check
             'ctype',
             'json'
         ]);
-
+        $this->extensions = $this->extensions->merge($this->getExtensionsRequiredInComposerFile());
+        $this->extensions = $this->extensions->unique();
         $this->extensions = $this->extensions->reject(function ($ext) {
             return extension_loaded($ext);
         });
 
         return $this->extensions->isEmpty();
     }
+
+    /**
+     * @return array
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function getExtensionsRequiredInComposerFile()
+    {
+        $installedPackages = json_decode($this->filesystem->get(base_path('vendor/composer/installed.json')), true);
+
+        $extensions = [];
+        foreach ($installedPackages as $installedPackage) {
+            $filtered = array_where(array_keys(array_get($installedPackage, 'require', [])), function ($value, $key) {
+                return starts_with($value, self::EXT);
+            });
+            foreach ($filtered as $extension) {
+                $extensions[] = str_replace_first(self::EXT, '', $extension);
+            }
+        }
+        return array_unique($extensions);
+    }
+
 }
