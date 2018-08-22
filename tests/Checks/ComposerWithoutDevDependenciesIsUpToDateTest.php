4b0636b9e3d105e34b1193d7e4cd8b51340b2587
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-08-22 23:17:36 +0200

    Test the ComposerWithoutDevDependenciesIsUpToDate check

---
+++ tests/Checks/ComposerWithoutDevDependenciesIsUpToDateTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace BeyondCode\SelfDiagnosis\Tests\Checks;
+
+use Orchestra\Testbench\TestCase;
+use BeyondCode\SelfDiagnosis\Composer;
+use BeyondCode\SelfDiagnosis\Checks\ComposerWithoutDevDependenciesIsUpToDate;
+
+class ComposerWithoutDevDependenciesIsUpToDateTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_checks_that_composer_is_up_to_date()
+    {
+        $composer = \Mockery::mock(Composer::class);
+        $composer->shouldReceive('setWorkingPath');
+        $composer->shouldReceive('installDryRun')->once()->andReturn('Nothing to install or update');
+
+        $check = new ComposerWithoutDevDependenciesIsUpToDate($composer);
+
+        $this->assertTrue($check->check([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_detects_that_composer_isnt_up_to_date()
+    {
+        $composer = \Mockery::mock(Composer::class);
+        $composer->shouldReceive('setWorkingPath');
+        $composer->shouldReceive('installDryRun')->once()->andReturn('Something to update');
+
+        $check = new ComposerWithoutDevDependenciesIsUpToDate($composer);
+
+        $this->assertFalse($check->check([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_use_the_additional_options()
+    {
+        $additional = str_random();
+
+        $composer = \Mockery::mock(Composer::class);
+        $composer->shouldReceive('setWorkingPath');
+        $composer->shouldReceive('installDryRun')->once()->withArgs(function ($arg) use ($additional) {
+            return str_contains($arg, $additional);
+        })->andReturn('Nothing to install or update');
+
+        $check = new ComposerWithoutDevDependenciesIsUpToDate($composer);
+
+        $this->assertTrue($check->check([
+            'additional_options' => $additional,
+        ]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_name_for_the_check()
+    {
+        $check = app(ComposerWithoutDevDependenciesIsUpToDate::class);
+        $this->assertInternalType('string', $check->name([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_message_for_the_check()
+    {
+        $check = app(ComposerWithoutDevDependenciesIsUpToDate::class);
+        $this->assertInternalType('string', $check->message([]));
+    }
+}
