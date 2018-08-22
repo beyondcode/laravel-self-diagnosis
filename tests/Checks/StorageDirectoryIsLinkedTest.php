f8759c5b1c7801358c41d18c332d318b5012d18d
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-08-22 22:53:10 +0200

    Cover the message and name function from StorageDirectoryIsLinked in a test and update the valid check test with a correct mock result

--- tests/Checks/StorageDirectoryIsLinkedTest.php
+++ tests/Checks/StorageDirectoryIsLinkedTest.php
@@ -15,10 +15,60 @@ class StorageDirectoryIsLinkedTest extends TestCase
 
         $filesystem->shouldReceive('isDirectory')
             ->with(public_path('storage'))
-            ->andReturn('link');
+            ->andReturn(true);
 
         $check = new StorageDirectoryIsLinked($filesystem);
 
         $this->assertTrue($check->check([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_checks_if_the_storage_directory_isnt_linked()
+    {
+        $filesystem = \Mockery::mock(Filesystem::class);
+
+        $filesystem->shouldReceive('isDirectory')
+            ->with(public_path('storage'))
+            ->andReturn(false);
+
+        $check = new StorageDirectoryIsLinked($filesystem);
+
+        $this->assertFalse($check->check([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_failed_nice_if_the_storage_directory_check_failed()
+    {
+        $filesystem = \Mockery::mock(Filesystem::class);
+
+        $filesystem->shouldReceive('isDirectory')
+            ->with(public_path('storage'))
+            ->andThrow(\Exception::class, 'testexception');
+
+        $check = new StorageDirectoryIsLinked($filesystem);
+
+        $this->assertFalse($check->check([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_name_for_the_check()
+    {
+        $check = app(StorageDirectoryIsLinked::class);
+        $this->assertInternalType('string', $check->name([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_message_for_the_check()
+    {
+        $check = app(StorageDirectoryIsLinked::class);
+        $this->assertInternalType('string', $check->message([]));
     }
 }
