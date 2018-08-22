df58978ae104974779ed9c05899a852352f028c7
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-08-22 21:43:56 +0200

    Expand the test for MigrationsAreUpToDate and cover the message and name function

--- tests/Checks/MigrationsAreUpToDateTest.php
+++ tests/Checks/MigrationsAreUpToDateTest.php
@@ -32,4 +32,41 @@ class MigrationsAreUpToDateTest extends TestCase
 
         $this->assertFalse($check->check([]));
     }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_name_for_the_check()
+    {
+        $check = app(MigrationsAreUpToDate::class);
+        $this->assertInternalType('string', $check->name([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_message_for_the_check()
+    {
+        $check = app(MigrationsAreUpToDate::class);
+        $this->assertInternalType('string', $check->message([]));
+    }
+
+    /**
+     * @test
+     *
+     * @depends it_returns_a_message_for_the_check
+     */
+    public function it_detects_that_migrations_check_failed()
+    {
+        $check = new MigrationsAreUpToDate();
+
+        $firstMessage = $check->message([]);
+
+        Artisan::shouldReceive('call')->andThrow(\PDOException::class, 'testmessage');
+
+        $this->assertFalse($check->check([]));
+
+        $message = $check->message([]);
+        $this->assertNotEquals($message, $firstMessage);
+    }
 }
