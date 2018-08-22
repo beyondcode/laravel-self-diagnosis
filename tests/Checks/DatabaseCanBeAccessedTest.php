a3f1e9e2de1aceeb6b49116f7c9134eca1e8558b
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-08-22 21:26:45 +0200

    Cover the message and name function from DatabaseCanBeAccessed in a test and also test the custom database connections config

--- tests/Checks/DatabaseCanBeAccessedTest.php
+++ tests/Checks/DatabaseCanBeAccessedTest.php
@@ -19,6 +19,48 @@ class DatabaseCanBeAccessedTest extends TestCase
 
         DB::shouldReceive('connection')->andReturn($mock);
 
-        $this->assertTrue($check->check([]));
+        $this->assertTrue($check->check([]), 'The check should success but has failed');
+    }
+
+    /** @test */
+    public function it_checks_db_access_for_configured_connections()
+    {
+        $check = app(DatabaseCanBeAccessed::class);
+        $this->assertFalse($check->check([]));
+
+        $mock = \Mockery::mock(\Illuminate\Database\Connection::class);
+        $mock->shouldReceive('getPdo');
+
+        // the first call is the default
+        DB::shouldReceive('connection')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($mock);
+
+        // the second call is the connection that is configured
+        DB::shouldReceive('connection')
+            ->with('test')
+            ->once()
+            ->andReturn($mock);
+
+        $this->assertTrue($check->check(['connections' => ['test']]), 'The check should success but has failed');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_name_for_the_check()
+    {
+        $check = app(DatabaseCanBeAccessed::class);
+        $this->assertInternalType('string', $check->name([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_message_for_the_check()
+    {
+        $check = app(DatabaseCanBeAccessed::class);
+        $this->assertInternalType('string', $check->message([]));
     }
 }
