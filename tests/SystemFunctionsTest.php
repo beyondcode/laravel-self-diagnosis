e1723afb503d13e0a658ea512d051ef54cfb77d4
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-10-01 21:51:08 +0200

    Remove the incomplete tests in the SystemFuntionsTest and the fixtures for that tests

--- tests/SystemFunctionsTest.php
+++ tests/SystemFunctionsTest.php
@@ -10,30 +10,6 @@ class SystemFunctionsTest extends TestCase
     /**
      * @test
      */
-    public function it_find_windows_as_os()
-    {
-        $this->markTestIncomplete('Using the const is very difficult to test');
-        require __DIR__ . '/fixtures/windowsconst.php';
-
-        $systemFunctions = new SystemFunctions();
-        $this->assertTrue($systemFunctions->isWindowsOperatingSystem());
-    }
-
-    /**
-     * @test
-     */
-    public function it_find_nowindows_as_os()
-    {
-        $this->markTestIncomplete('Using the const is very difficult to test');
-        require __DIR__ . '/fixtures/linuxconst.php';
-
-        $systemFunctions = new SystemFunctions();
-        $this->assertFalse($systemFunctions->isWindowsOperatingSystem());
-    }
-
-    /**
-     * @test
-     */
     public function it_find_an_available_function()
     {
         $systemFunctions = new SystemFunctions();
--- tests/fixtures/windowsconst.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace BeyondCode\SelfDiagnosis;
-
-// used to test the BeyondCode\SelfDiagnosis\SystemFunctions::isWindowsOperatingSystem function
-const PHP_OS = 'WINDOWS';
