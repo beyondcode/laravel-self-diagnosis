84a5b25f0ca5c32eea8b9fee2edf399a52ce0fcf
Tobias van Beek <t.vanbeek@tjvb.nl>
2018-10-01 21:31:38 +0200

    Add a test for the RoutesAreNotCached check

---
+++ tests/Checks/RoutesAreNotCachedTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BeyondCode\SelfDiagnosis\Tests\Checks;
+
+use Orchestra\Testbench\TestCase;
+use BeyondCode\SelfDiagnosis\Checks\RoutesAreNotCached;
+
+/**
+ * @group checks
+ */
+class RoutesAreNotCachedTest extends TestCase
+{
+    /**
+     * be sure toe remove the cached config
+     *
+     * {@inheritDoc}
+     * @see \Orchestra\Testbench\TestCase::tearDown()
+     */
+    public function tearDown()
+    {
+        @unlink(app()->getCachedRoutesPath());
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_detect_the_cached_config()
+    {
+        touch(app()->getCachedRoutesPath());
+        $check = new RoutesAreNotCached();
+
+        $this->assertFalse($check->check([]), 'The cache file isn\'t created but we get that it is created');
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_detect_the_not_cached_config()
+    {
+        @unlink(app()->getCachedRoutesPath());
+        $check = new RoutesAreNotCached();
+
+        $this->assertTrue($check->check([]), 'The cache file is created but we get that it isn\'t created');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_name_for_the_check()
+    {
+        $check = new RoutesAreNotCached();
+        $this->assertInternalType('string', $check->name([]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_message_for_the_check()
+    {
+        $check = new RoutesAreNotCached();
+        $this->assertInternalType('string', $check->message([]));
+    }
+}
